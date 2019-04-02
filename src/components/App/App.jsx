d85a1f81f552e953302dfb2b063b5efd0c9dd4e2
VamOSGS <gegham.samvelyan@hotmail.com>
2019-04-03 00:19:27 +0400

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './AppStyles.less';
+
+const App = () => (
+  <div className="App">
+    <h1 className="title">React-avate</h1>
+  </div>
+);
+
+export default App;
---
+++ src/components/Root/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { hot } from 'react-hot-loader';
+import App from '../App';
+import store from '../../store';
+
+const Root = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+export default hot(module)(Root);
